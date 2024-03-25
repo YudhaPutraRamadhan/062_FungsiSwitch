c8642945380d5954cd646582d279d7a5c463fe99
MzSuwawa <yudharamadhan838@gmail.com>
2024-03-25 14:24:15 +0700

    Prosedur Input

--- Fungsi Switch/Fungsi Switch.cpp
+++ Fungsi Switch/Fungsi Switch.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 int bilSatu, bilDua;
+
+void input()
+{
+	cout << "Masukkan Bilangan Pertama = ";
+	cin >> bilSatu;
+	cout << "Masukkan Bilangan Kedua = ";
+	cin >> bilDua;
+}
