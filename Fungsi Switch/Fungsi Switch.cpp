82e0b78465c7acf826254c2cbb808321cf8cd904
MzSuwawa <yudharamadhan838@gmail.com>
2024-03-25 14:41:50 +0700

    Prosedur menentukan pilihan

--- Fungsi Switch/Fungsi Switch.cpp
+++ Fungsi Switch/Fungsi Switch.cpp
@@ -30,3 +30,21 @@ float pembagian(float a, float b)
 {
 	return a / b;
 }
+
+int main()
+{
+	int pilihan;
+
+	do
+	{
+		cout << "===============================" << endl;
+		cout << "MENU" << endl;
+		cout << "1.Penjumlahan " << endl;
+		cout << "2.Pengurangan " << endl;
+		cout << "3.Perkalian " << endl;
+		cout << "4.Pembagian " << endl;
+		cout << "5.Exit" << endl;
+		cout << "Masukkan pilihan 1/2/3/4/5 = ";
+		cin >> pilihan;
+	} while (pilihan != 5);
+}
